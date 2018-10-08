ef1a5a86b971b2a89eba815d571d547841070427
yihongou <yihongou@tcl.com>
2018-10-08 18:07:53 +0800

    增加规格选项

--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -1,15 +1,16 @@
 package com.pinyougou.sellergoods.service.impl;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-import com.pinyougou.mapper.TbGoodsDescMapper;
+import com.alibaba.fastjson.JSON;
+import com.pinyougou.mapper.*;
+import com.pinyougou.pojo.*;
 import com.pinyougou.pojogroup.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.pinyougou.mapper.TbGoodsMapper;
-import com.pinyougou.pojo.TbGoods;
-import com.pinyougou.pojo.TbGoodsExample;
 import com.pinyougou.pojo.TbGoodsExample.Criteria;
 import com.pinyougou.sellergoods.service.GoodsService;
 
@@ -28,12 +29,77 @@ public class GoodsServiceImpl implements GoodsService {
 	@Autowired
 	private TbGoodsDescMapper goodsDescMapper;
 
+	@Autowired
+	private TbItemMapper itemMapper;
+
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+
+	@Autowired
+	private TbBrandMapper brandMapper;
+
+	@Autowired
+	private TbSellerMapper sellerMapper;
+
 	@Override
 	public void add(Goods goods) {
 		goods.getGoods().setAuditStatus("0");//设置未申请状态
 		goodsMapper.insert(goods.getGoods());
 		goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());//设置Id
 		goodsDescMapper.insert(goods.getGoodsDesc());//插入商品扩展数据
+
+		if ("1".equals(goods.getGoods().getIsEnableSpec())){
+			for (TbItem item : goods.getItemList()){
+				//构建标题  SPU名称+ 规格选项值
+				String title = goods.getGoods().getGoodsName();
+				Map<String,Object> map = JSON.parseObject(item.getSpec());
+				for (String key : map.keySet()){
+					title+= " " + map.get(key);
+				}
+				item.setTitle(title);
+
+				setItemValues(item,goods);
+
+				itemMapper.insert(item);
+			}
+		} else {//没有启用规格
+			TbItem item=new TbItem();
+			item.setTitle(goods.getGoods().getGoodsName());//标题
+			item.setPrice(goods.getGoods().getPrice());//价格
+			item.setNum(99999);//库存数量
+			item.setStatus("1");//状态
+			item.setIsDefault("1");//默认
+			item.setSpec("{}");//规格
+
+			setItemValues(item,goods);
+
+			itemMapper.insert(item);
+		}
+	}
+
+	private void setItemValues(TbItem item, Goods goods) {
+		//商品分类
+		item.setCategoryid(goods.getGoods().getCategory3Id());
+		item.setCreateTime(new Date());
+		item.setUpdateTime(new Date());
+
+		item.setGoodsId(goods.getGoods().getId());
+		item.setSellerId(goods.getGoods().getSellerId());
+		//分类名称
+		TbItemCat  itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
+		item.setCategory(itemCat.getName());
+		//品牌名称
+		TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
+		item.setBrand(brand.getName());
+		//商家名称(店铺名称)
+		TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
+		item.setSeller(seller.getNickName());
+		//图片
+		List<Map> imageList = JSON.parseArray(goods.getGoodsDesc().getItemImages(),Map.class);
+		if (imageList.size()>0){
+			item.setImage( (String)imageList.get(0).get("url"));
+		}
+
 	}
 
 	/**
--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -109,7 +109,7 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 		
 	}
 	
-	$scope.entity={ goodsDesc:{itemImages:[]}  };
+	$scope.entity={ goodsDesc:{itemImages:[],specificationItems:[]}  };
 	
 	//将当前上传的图片实体存入图片列表
 	$scope.add_image_entity=function(){
@@ -180,5 +180,58 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 		}
 	);
     });
+
+    
+    $scope.updateSpecAttribute=function ($event, name, value) {
+		var object = $scope.searchObjectByKey($scope.entity.goodsDesc.specificationItems ,'attributeName', name);
+
+		if (object!=null){
+			if ($event.target.checked){
+				object.attributeValue.push(value);
+			}else{//取消勾选
+				object.attributeValue.splice(object.attributeValue.indexOf(value) , 1);//移除选项
+                //如果选项都取消了，将此条记录移除
+                if(object.attributeValue.length==0){
+                    $scope.entity.goodsDesc.specificationItems.splice(
+                        $scope.entity.goodsDesc.specificationItems.indexOf(object),1);
+                }
+			}
+		} else {
+            $scope.entity.goodsDesc.specificationItems.push({"attributeName":name,"attributeValue":[value]});
+		}
+    }
+
+	//创建SKU列表
+    $scope.createItemList=function(){
+
+        $scope.entity.itemList=[{spec:{},price:0,num:99999,status:'0',isDefault:'0'} ];//列表初始化
+
+        var items= $scope.entity.goodsDesc.specificationItems;
+
+        for(var i=0;i<items.length;i++){
+            $scope.entity.itemList= addColumn( $scope.entity.itemList, items[i].attributeName,items[i].attributeValue );
+        }
+
+    }
+
+    addColumn=function(list,columnName,columnValues){
+
+        var newList=[];
+        for(var i=0;i< list.length;i++){
+            var oldRow=  list[i];
+            for(var j=0;j<columnValues.length;j++){
+                var newRow=  JSON.parse( JSON.stringify(oldRow)  );//深克隆
+                newRow.spec[columnName]=columnValues[j];
+                newList.push(newRow);
+            }
+        }
+        return newList;
+    }
+
+
+
+
+
+
 
 });	
